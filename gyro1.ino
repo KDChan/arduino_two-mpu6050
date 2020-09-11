e8b205f8c1c77283690c97c5065aea485f218c87
KimDongChan <70682288+ChanDongKim@users.noreply.github.com>
2020-09-11 14:29:37 +0900

    Update gyro1.ino

--- gyro1.ino
+++ gyro1.ino
@@ -1,3 +1,16 @@
+/* Copyright (C) 2012 Kristian Lauszus, TKJ Electronics. All rights reserved.
+  This software may be distributed and modified under the terms of the GNU
+  General Public License version 2 (GPL2) as published by the Free Software
+  Foundation and appearing in the file GPL2.TXT included in the packaging of
+  this file. Please note that GPL2 Section 2[b] requires that all works based
+  on this software must also be made publicly available under the terms of
+  the GPL2 ("Copyleft").
+  Contact information
+  -------------------
+  Kristian Lauszus, TKJ Electronics
+  Web : http://www.tkjelectronics.com
+  e-mail : kristianl@tkjelectronics.com
+*/
 
 /*
    손목 gyro2로부터 movcount 받음, 안드로이드와 통신 부분
